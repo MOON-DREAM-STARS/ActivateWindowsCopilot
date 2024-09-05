2623c41e837afcd3efb5da39cee0e717c5c85e8b
MDS <1976165080@qq.com>
2024-09-06 07:27:24 +0800

    fix bugs

--- ActivateWindowsCopilot.bat
+++ ActivateWindowsCopilot.bat
@@ -8,13 +8,19 @@ start ms-settings:taskbar
 REM 等待2秒以确保设置页面完全加载
 timeout /t 2
 
+REM 如果Windows中的Copilot（预览）已经启用，则关闭Windows中的Copilot（预览）【该操作的目的是刷新状态，有的时候Windows会有Bug】
+powershell -Command "Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'ShowCopilotButton' -Value 0"
+
 REM 启用Windows中的Copilot（预览）
 powershell -Command "Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'ShowCopilotButton' -Value 1"
 
 echo Copilot is enabled now.
 
-REM 关闭设置页面
-taskkill /f /im SystemSettings.exe
+REM 等待2秒以确保设置页面完全加载
+timeout /t 2
+
+REM 关闭“个性化 > 任务栏 > 任务栏项”页面
+powershell -Command "Stop-Process -Name SystemSettings"
 
 REM 关闭.bat窗口
 exit
